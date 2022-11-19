41b67568db6cc208dc113a46778a76f7cd114243
itachi-13 <amierib013@gmail.com>
2022-11-19 16:47:51 +0300

    update

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ const close = document.getElementById('close');
 const ulMenu = document.querySelector('.ul-menu');
 const opacity = document.getElementById('opacity');
 
+const cart = document.querySelector('#icon-cart');
+const cartEmpty = document.querySelector('#cart-empty');
+const cartCheckout = document.querySelector('#cart-checkout');
+const deleteIcon = document.querySelector('#deleteIcon');
+let count = 0;
+
 const smallImg = document.querySelectorAll('.four-img-self img');
 const mainImg = document.querySelector('.left > img');
 
 const showImg = document.querySelector('.show-img');
 
-const smallImgShadow = document.querySelectorAll('.four-img-self-shadow img')
-const mainImgShadow = document.getElementById('main-img-shadow')
+const smallImgShadow = document.querySelectorAll('.four-img-self-shadow img');
+const mainImgShadow = document.getElementById('main-img-shadow');
 
 const prevMobile = document.getElementById('prev-mobile');
 const nextMobile = document.getElementById('next-mobile');
@@ -18,6 +24,8 @@ const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 
 const closeShadow = document.getElementById('close-shadow');
+
+const addToCart = document.querySelector('.add-to-cart');
 
 let orderMobileImg = 0;
 
@@ -29,6 +37,13 @@ iconMenu.addEventListener('click', () => {
 close.addEventListener('click', () => {
     ulMenu.style.width = '0';
     opacity.style.display = 'none';
+})
+
+deleteIcon.addEventListener('click', () => {
+    count = 0;
+    cart.setAttribute('cart', count);
+    cartEmpty.style.zIndex = 6;
+
 })
 
 smallImg.forEach((item, i) => {
@@ -69,13 +84,24 @@ prev.addEventListener('click', () => {
 mainImg.addEventListener('click', () => {
     if (window.innerWidth > 420) {
         opacity.style.display = 'block';
-         showImg.style.display = 'flex';
+        showImg.style.display = 'flex';
     }
 })
 
 closeShadow.addEventListener('click', () => {
     opacity.style.display = 'none';
     showImg.style.display = 'none';
+})
+
+addToCart.addEventListener('click', () => {
+    count = document.querySelector('#count-number').textContent;
+    if (count > 0) {
+        cart.setAttribute('cart', count);
+        cartEmpty.style.zIndex = 4;
+        cartCheckout.style.zIndex = 5;
+        document.querySelector('.count-in-cart-checkout').textContent = count;
+        document.querySelector('.total-price').textContent = `$${125 * count}.00`;
+    }
 })
 
 function change_small_img(type, item) {
@@ -98,16 +124,16 @@ function change_order(a, b, c) {
 }
 
 function add_number() {
-    let number = document.getElementById('count-number').textContent;
-    number ++;
-    document.getElementById('count-number').textContent = number;
+    count = document.querySelector('#count-number').textContent;
+    count++;
+    document.getElementById('count-number').textContent = count;
 }
 
 function minus_number() {
-    let number = document.getElementById('count-number').textContent;
-    if (number > 0) {
-      number --;
-      document.getElementById('count-number').textContent = number;        
+    count = document.querySelector('#count-number').textContent;
+    if (count > 0) {
+        count--;
+        document.getElementById('count-number').textContent = count;
     }
 
 }
